ee517393dcec9d04086523aa778657291cd70b6f
websites-2025 <websites-2025@outlook.com>
2025-08-10 00:37:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// script.js
-document.getElementById('clickButton').addEventListener('click', function() {
-    alert('¡Has hecho clic en el botón!');
+// Script para manejar el envío del formulario
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Aquí puedes agregar la lógica para enviar el formulario, como una llamada AJAX o redirigir a una página de gracias.
+    alert('¡Gracias por contactarnos!');
 });
